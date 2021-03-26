16bb1fcc6d8ec5d959aeace66256029bf45e45af
Damian <damiangrygiercz@gmail.com>
2021-03-26 09:59:56 +0100

    nwm naprawa

--- md/Program.cs
+++ md/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace md
 {
@@ -9,6 +10,7 @@ namespace md
     {
         static void Main(string[] args)
         {
+            int[] tableweight = new int[100];
             List<string> test = new List<string>(){ "12","25","32","55","87","98","55","43","10","18"};
 
             string filename = "test.txt"; 
@@ -22,20 +24,36 @@ namespace md
             int temp = 9;
             for (int i = 1; i < 10; i++)
             {
-                
-                for (int j = i+1; j < 10; j++)
+                for (int j = i + 1; j < 10; j++)
                 {
                     temp += 1;
                     weight[i, j] = temp;
                     weight[j, i] = temp;
                 }
             }
+
+            for (int i = 0; i < 10; i++)
+            {
+                weight[0, i] = i;
+                weight[i, 0] = i;
+            }
+
+            //wypisywanie tabeli wag
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"{weight[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+
+
            
             for (int c = 0; c < 100; c++)
             {
-
-
                 int treeLenght = 10;
+                int sum = 0;
                 var R = new List<int>();
                 var T = new List<int>();
                 var wyn = new List<string>();
@@ -46,6 +64,7 @@ namespace md
                     R.Add(i + 1);
                 }
                 
+
                 for (int i = 0; i < treeLenght; i++)
                 {
                     var x = rnd.Next(0, R.Count);
@@ -55,19 +74,20 @@ namespace md
                         int a;
                         int b = R[x];
                         a = T[rnd.Next(T.Count)];
-
+                        sum += weight[-1+a, -1+b];
                         wyn.Add("{" + a + " " + b + "} ");
-
                     }
 
                     T.Add(R[x]);
                     R.RemoveAt(x);
                 }
 
+                tableweight[c] = sum;
                 foreach (var x in wyn)
                 {
-                    Console.Write(x);
+                    Console.Write(x); 
                     sb.Append($"{x} ");
+                    
                 }
 
                 Console.WriteLine();
@@ -77,6 +97,19 @@ namespace md
                 File.WriteAllLines(path,lines);
                 sb.Clear();
             }
+
+                
+                
+                         
+                var q =tableweight.Select((item,index)=> new{item,index}).OrderBy(x => x.item).Select(x=>x.index);
+                foreach (var i in q)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+         
+
+
         }
     }
-}
